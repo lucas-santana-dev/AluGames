3393acc78ea727fb4d572606adfdddb664f27aa4
Lucas Santana <admin@Mac-mini-de-Lucas.local>
2024-03-11 18:02:14 -0300

    Atualização  Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ import java.net.http.HttpResponse.BodyHandlers
 fun main() {
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=16"))
         .build()
 
     val response = client
